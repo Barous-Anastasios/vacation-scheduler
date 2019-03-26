003fcdbbb64d4e069f01ac2117478978678eef16
Tasos <Barous>
2019-03-26 23:41:15 +0200

    Initial Commit

---
+++ database/migrations/2019_03_26_211605_create_vacations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVacationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vacations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('start');
+            $table->date('ends');
+            $table->integer('status_id')->unsigned();
+            $table->text('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vacations');
+    }
+}
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Role;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $employee = Role::where('name', 'employee')->first();
+        $admin = Role::where('name', 'admin')->first();
+
+        $user = new User;
+        $user->name = 'Employee';
+        $user->email = 'employee@email.com';
+        $user->email_verified_at = now();
+        $user->password = bcrypt('123456');
+        $user->remember_token = Str::random(10);
+        $user->save();
+        $user->roles()->attach($employee);
+
+        $user = new User;
+        $user->name = 'Admin';
+        $user->email = 'admin@email.com';
+        $user->email_verified_at = now();
+        $user->password = bcrypt('123456');
+        $user->remember_token = Str::random(10);
+        $user->save();
+        $user->roles()->attach($admin);
+    }
+}
